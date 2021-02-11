0c5766c3b65c4544af6845627b59f53bdfd921fb
jerryliu321 <liuruijie846@gmail.com>
2021-02-11 15:24:34 +0800

    jerry

--- Chapter6/6-11.cpp
+++ Chapter6/6-11.cpp
@@ -1,8 +1,35 @@
 #include<iostream>
 using namespace std;
+void print_stuff(float data_to_ignore);
+void print_message(float list_this_data);
+void print_float(float data_to_print);
+void (*function_pointer)(float);
 
 int main()
 {
-  cout << "hello world" << endl;
+  float pi = (float)3.14159;
+  float two_pi = (float)2.0 * pi;
+  print_stuff(pi);
+  function_pointer = print_stuff;
+  function_pointer(pi);
+  function_pointer = print_message;
+  function_pointer(two_pi);
+  function_pointer(13.0);
+  function_pointer = print_float;
+  function_pointer(pi);
+  print_float(pi);
   return 0;
+}
+
+void print_stuff(float data_to_ignore)
+{
+  cout << "This is the print stuff function.\n";
+}
+void print_message(float list_this_data)
+{
+  cout << "this data will be listed is" << list_this_data <<endl;
+}
+void print_float(float data_to_print)
+{
+  cout << "this data to be printed is" << data_to_print <<endl;
 }
