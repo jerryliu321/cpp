c91555f703727070f451ac9672821647a2eb8f4c
jerryliu321 <liuruijie846@gmail.com>
2021-02-12 11:36:38 +0800

    jerry

--- Chapter6/6-13.cpp
+++ Chapter6/6-13.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
+class Point
+{
+private:
+  int X, Y;
+public:
+  Point(int xx=0, int yy=0){X=xx; Y=yy;}
+  int GetX() {return X;}
+  int GetY() {return Y;}
+};
+
 int main()
 {
+  Point A(4,5);
+  Point *p1=&A;
+  int (Point::*p_GetX)() = Point::GetX;
+
+  cout << (A.*p_GetX)() << endl;
+  cout << p1->GetX() << endl;
+  cout << A.GetX() << endl;
   return 0;
 }
