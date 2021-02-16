dbd721c9efde86d15788c0044a85f705058bbbc4
jerryliu321 <liuruijie846@gmail.com>
2021-02-16 19:11:38 +0800

    jerry

--- Chapter7/7-6.cpp
+++ Chapter7/7-6.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 using namespace std;
+class B1
+{
+    public:
+        int nV;
+        void func() {cout << "Member of B1" << endl;}
+};
+
+class B2
+{
+    public:
+        int nV;
+        void func() {cout << "Member of B2" << endl;}
+};
+class D1:public B1, public B2
+{
+    public:
+        int nV;
+        void func() {cout << "Member of D1" << endl;}
+};
 
 int main()
 {
+    D1 d1;
+    d1.nV = 1;
+    d1.func();
+    d1.B1::nV=2;
+    d1.B1::func();
+    d1.B2::nV=2;
+    d1.B2::func();
     return 0;
 }
